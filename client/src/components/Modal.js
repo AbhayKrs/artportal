20e8051884b35773d37cfb0b0783df8f09e4338d
Abhay Kumar <jayabhay9@gmail.com>
2022-08-19 23:15:57 +0530

    Updated Token modal size

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -310,7 +310,7 @@ export const TokenModal = (props) => {
 
     return (
         <div className={`${open ? 'block' : 'hidden'} flex fixed w-full inset-0 z-50 overflow-hidden justify-center items-center animated fadeIn faster`} style={{ background: 'rgba(0, 0, 0, .7)' }}>
-            <div className="relative m-auto bg-slate-100 dark:bg-neutral-800 lg:w-8/12 sm:w-11/12 xs:w-11/12 rounded-xl z-50 overflow-y-auto">
+            <div className="relative m-auto bg-slate-100 dark:bg-neutral-800 lg:w-5/12 sm:w-11/12 xs:w-11/12 rounded-xl z-50 overflow-y-auto">
                 <div className='grid'>
                     <div className='p-4 flex flex-col'>
                         <IoCloseSharp onClick={onClose} className='w-7 h-7 absolute top-0 right-0 mt-2 mr-2 cursor-pointer text-gray-400' />
