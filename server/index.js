f8018347b28ab87070eea290ce63e20036d25915
Abhay <jayabhay9@gmail.com>
2022-12-25 17:34:18 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -70,4 +70,4 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running`));
+app.listen(PORT, console.log(`Server running`, PORT));
